7df4fa28531e2d150d897b600cbada56fda40fbf
hagmannstephan <stephanhagmann@outlook.com>
2025-06-24 14:23:06 +0200

    style: added base styling to style.scss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  const navigateToExample = () => {
+    window.location.href = 'https://example.com'
+  }
 </script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+  <div class="container area">
+    <p>Text in seperated area</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <a href="https://example.com" target="_blank" rel="noopener">This is a link</a>
+  <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Ut hendrerit semper vel class aptent taciti <a href="https://example.com" target="_blank" rel="noopener">inline link</a> sociosqu.</p>
+  <br>
+  <button @click="navigateToExample">This is a button</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
